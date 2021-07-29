859ce1222ab4c27c81198b73b4598d3feda3fdba
Lars Passic <larspassic@outlook.com>
2021-07-28 20:24:34 -0700

    Working on Entity Framework labs

---
+++ Labs/SchoolEF/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
