f99654db9ddce3dd035a82ccb1bb839b67fb5d40
Lars Passic <larspassic@outlook.com>
2021-08-10 16:30:30 -0700

    Working on project

---
+++ Homework/The Learning Center/Website/DatabaseAccessor.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace Website
+//{
+//    public class DatabaseAccessor
+//    {
+//        //I think this is the default constructor
+//        static DatabaseAccessor()
+//        {
+//            Instance = new MiniCstructorDbContext();
+//        }
+
+//        public static MiniCstructorDbContext instance { get; private set; }
+//    }
+//}
---
+++ Homework/The Learning Center/Website/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace Website
+//{
+//    public interface IUserRepository
+//    {
+//        UserModel LogIn(string email, string password);
+//        UserModel Register(string email, string password);
+//    }
+
+
+//    public class UserModel
+//    {
+//        public int Id { get; set; }
+//        public string Name { get; set; }
+
+//    }
+
+//    //This is a class that derives from an interface
+//    public class UserRepository : IUserRepository
+//    {
+//        //This is a method
+//        public UserModel LogIn(string email, string password)
+//        {
+//            var user = DatabaseAccessor.Instance.user.FirstOrDefault(t => t.UserEmail.ToLower() == email.ToLower() && t.UserHashedPassword == password);
+
+//            if (user == null)
+//            {
+//                return null;
+//            }
+
+//            return new UserModel { Id = user.UserId, Name = user.UserEmail };
+//        }
+
+//        //This is a method
+//        public UserModel Register(string email, string password)
+//        {
+//            var user = DatabaseAccessor.Instance.User.Add(new Database.User
+//            {
+//                UserEmail = email,
+//                UserHashedPassword = password
+//            });
+
+//            DatabaseAccessor.Instance.SaveChanges();
+
+//            return new UserModel { Id = user.Entity.UserId, Name = user.Entity.UserEmail };
+//        }
+//    }
+//}
