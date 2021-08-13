d187d55aff07cdf1d6afb8a2c63a79c54fa1b4af
Lars Passic <larspassic@outlook.com>
2021-08-12 22:05:43 -0700

    Finished class labs and exercises for the night

---
+++ Labs/HelloWorldService/Exercise files/JSONexercise.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldService
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+        public bool isAlive;
+        public phoneNumber[] phoneNumbers;
+        public child[] children;
+        public Person spouse;
+    }
+
+    public class child
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+    }
+
+    public class phoneNumber
+    {
+        public string type;
+        public string number;
+    }
+}
--- Labs/HelloWorldService/Startup.cs
+++ Labs/HelloWorldService/Startup.cs
@@ -24,7 +24,11 @@ namespace HelloWorldService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services
+                .AddMvc()
+                .AddXmlSerializerFormatters()
+                //OR .AddXmlDataContractSerializerFormatters()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
